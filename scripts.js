7bfac39109f14d561b944491ce16b75cffb1ba84
agbegin <90297599+agbegin@users.noreply.github.com>
2023-04-21 14:04:43 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,3 +21,26 @@ document.addEventListener("DOMContentLoaded", function() {
     window.prevSlide = prevSlide;
     window.nextSlide = nextSlide;
 });
+
+function createDots() {
+    const carouselIndicators = document.querySelector('.carousel-indicators');
+    carouselItems.forEach((item, index) => {
+        const dot = document.createElement('button');
+        dot.classList.add('dot');
+        if (index === 0) dot.classList.add('active');
+        dot.addEventListener('click', () => goToSlide(index));
+        carouselIndicators.appendChild(dot);
+    });
+}
+
+function updateDots(slideIndex) {
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[slideIndex].classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    createDots();
+    // ...
+});
+
