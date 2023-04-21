2599e3c3388200c8211d437621f3a6bd87ce1ab3
agbegin <90297599+agbegin@users.noreply.github.com>
2023-04-21 08:53:50 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+const carouselItems = document.querySelectorAll(".carousel-item");
+
+function goToSlide(slideIndex) {
+    carouselItems[currentSlide].classList.remove("active");
+    currentSlide = (slideIndex + carouselItems.length) % carouselItems.length;
+    carouselItems[currentSlide].classList.add("active");
+    const carouselInner = document.querySelector(".carousel-inner");
+    carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function prevSlide() {
+    goToSlide(currentSlide - 1);
+}
+
+function nextSlide() {
+    goToSlide(currentSlide + 
