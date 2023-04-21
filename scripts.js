f414652e156b74bd1ef3b14deba86f7427454cd5
agbegin <90297599+agbegin@users.noreply.github.com>
2023-04-21 11:15:17 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let currentSlide = 0;
+    const carouselItems = document.querySelectorAll(".carousel-item");
 
+    function goToSlide(slideIndex) {
+        carouselItems[currentSlide].classList.remove("active");
+        currentSlide = (slideIndex + carouselItems.length) % carouselItems.length;
+        carouselItems[currentSlide].classList.add("active");
+        const carouselInner = document.querySelector(".carousel-inner");
+        carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
+    }
 
-let currentSlide = 0;
-const carouselItems = document.querySelectorAll(".carousel-item");
+    function prevSlide() {
+        goToSlide(currentSlide - 1);
+    }
 
-function goToSlide(slideIndex) {
-    carouselItems[currentSlide].classList.remove("active");
-    currentSlide = (slideIndex + carouselItems.length) % carouselItems.length;
-    carouselItems[currentSlide].classList.add("active");
-    const carouselInner = document.querySelector(".carousel-inner");
-    carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
+    function nextSlide() {
+        goToSlide(currentSlide + 1);
+    }
 
-function prevSlide() {
-    goToSlide(currentSlide - 1);
-}
-
-function nextSlide() {
-    goToSlide(currentSlide +1) 
-
-              });
+    window.prevSlide = prevSlide;
+    window.nextSlide = nextSlide;
+});
